c0d0f9f5565cc6ffae97d757faa18712556336a3
Luke Prananta <lukeprananta@hotmail.com>
2015-11-26 03:16:10 +0100

    Filtering functionality added, edit task redesigned/rewritten, refactoring, initial css added (disabled for now)

--- js/note.js
+++ js/note.js
@@ -8,22 +8,16 @@ function Task(note, date, priority, id) {
 }
 
 Task.prototype.getTask = function() {
-var String = '<div id="task' + this.id + '" class="note" priority="' + priority(this.priority) + '" date="' + this.date + '"><h3><input type="checkbox" id=' 
-		+ this.id + ' value="selected">' + this.note + '</h3><p>Due Date: ' 
-		+ this.date + '</p><p>Priority: ' + priority(this.priority) + '</p><p>Completed: ' 
-		+ this.done + '</p></div>';
+var String = '<div id="task' + this.id + '" class="task" priority="' + priority(this.priority) + '" date="' + this.date + '"><input type="checkbox" class="task-select"><h3>' 
+		+ this.note + '</h3><div class="task-details"><br>Due Date: ' 
+		+ this.date + ' Priority: ' + priority(this.priority) + ' Completed: ' 
+		+ this.done + '</br></div></div>';
 	return String;
 }
 
-Task.prototype.delete = function() {
-	var div = document.getElementById( "task" + this.id);
-	div.parentNode.removeChild(div);
-}
-
-Task.prototype.finished = function() {
-	this.done = true;
-}
-
+Task.prototype.add = function(list) { list.append(this.getTask()); $('#task' + this.id).find('.task-details').hide(); }
+Task.prototype.finished = function() { this.done = true; }
+Task.prototype.delete = function() { $('#task' + this.id).remove(); }
 Task.prototype.update = function() {$('#task' + this.id).replaceWith(this.getTask());}
 Task.prototype.getNote = function () {return this.note;}
 Task.prototype.setNote = function (note) {this.note = note;}
